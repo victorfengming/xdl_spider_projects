88d2b122c9463618b9d6a2a013554494e5c052b2
victor <fengming19981221@163.com>
2019-11-17 17:40:34 +0800

    __init__

---
+++ day05/lol_test.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Created by 秋叶夏风
+
+# 本模块的功能:<用于练习xpath>
+
+from requests import get
+调用xpath的包
+
+url = "http://lol.178.com/"
+
+response = get(url)
+
+# with open("178.html",'wb') as f:
+#     f.write(response.content)
+
+# 需要找到的是,/html/head/title
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+       ┌─┐       ┌─┐ + +
+    ┌──┘ ┴───────┘ ┴──┐++
+    │                 │
+    │       ───       │++ + + +
+    ███████───███████ │+
+    │                 │+
+    │       ─┴─       │
+    │                 │
+    └───┐         ┌───┘
+        │         │
+        │         │   + +
+        │         │
+        │         └──────────────┐
+        │                        │
+        │                        ├─┐
+        │                        ┌─┘
+        │                        │
+        └─┐  ┐  ┌───────┬──┐  ┌──┘  + + + +
+          │ ─┤ ─┤       │ ─┤ ─┤
+          └──┴──┘       └──┴──┘  + + + +
+                 神兽保佑
+                代码无BUG!
+
+
+'''
