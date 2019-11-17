475c69edfe73370cdc6122e526ab0584d72bd991
victor <fengming19981221@163.com>
2019-11-17 23:56:07 +0800

    审查元素和查看网页源代码是不一样的

--- day05/lol_test.py
+++ day05/lol_test.py
@@ -32,23 +32,77 @@ html_ele = etree.HTML(response.text)
 # 注意,最前面要是有一个/的话,就是从根目录开始
 # 绝对路径方式这是
 
-meta_ele = html_ele.xpath('/html/head/meta[1]')
-print(meta_ele[0])
+# meta_ele = html_ele.xpath('/html/head/meta[1]')
+# print(meta_ele[0])
 
 # 在Xpath中可以通过索引的方式获取具体的哪个标签
 # 在python中从0开始,这里从1,开始
 
-charset_str = html_ele.xpath('/html/head/meta[1]/@charset')
+# charset_str = html_ele.xpath('/html/head/meta[1]/@charset')
 
 # 通过@获取标签下面的属性
-print(charset_str[0])
+# print(charset_str[0])
 # 记住,所有的xpath都会给我们返回一个列表,要在取第几个才行
 
 # /html/head/link[1]/@href
-href_str = html_ele.xpath("/html/head/link[1]/@href")
-print(href_str[0])
+# href_str = html_ele.xpath("/html/head/link[1]/@href")
+# print(href_str[0])
 
-# 谓词相关信息
+# 谓词相关信息'/html/body/div[@class="wrap"]/@class'
 # /html/body/div[2]
-div_ele = html_ele.xpath('/html/body/div[2]/@class')
-print(div_ele)
+# div_ele = html_ele.xpath('/html/body/div[1]/@class')
+# div_ele = html_ele.xpath('/html/body/div[@class="wrap"]/@class')
+
+# div_ele = html_ele.xpath('/html/body/div[@class="wrap"]/div[1]/div[@class="head"]/@class')
+
+# div_ele = html_ele.xpath('/html/body/div[1]/div/div[5]/div[3]/div[2]/ul[1]/li[2]/a/span[1]/text()')
+# print(div_ele[0])
+
+
+# /html/body/div[2]/div/div[5]/div[3]/div[2]/ul[1]/li[2]/a/span[1]
+
+# 搜索内容 //div[@class="head"]
+
+# div_ele = html_ele.xpath('//div[@class="head"]/div[1]/div[1]/a/text()')
+# print(div_ele)
+
+# div_ele = html_ele.xpath('//div[@class="Oldversion"]/a/text()')
+# div_ele = html_ele.xpath('//div[@class="Oldversion"]/a/@href')
+# print(div_ele)
+
+# 获取所有的属性包含 itemprop的meta标签: /html/head/meta[@itemprop]/@itemprop
+# 谓词
+# div_ele = html_ele.xpath('/html/head/meta[@itemprop]/@itemprop')
+# print(div_ele)
+
+# 获取所有的属性不包含 itemprop的meta标签: '/html/head/meta[not(@itemprop)]'
+# 需要使用not函数
+# 函数
+# div_ele = html_ele.xpath('/html/head/meta[not(@itemprop)]')
+# print(div_ele)
+
+
+# # 找到刚刚的ul,XPATH: //ul[@class="ul-nav-main"]//text()
+# div_ele = html_ele.xpath('//ul[@class="ui-nav-main"]//text()')
+# print(div_ele)
+#
+# # 列表推导式
+# strs = [item for item in div_ele if item.strip()]
+# print(strs)
+
+# contains()函数 包不包含什么
+# starts_with() 以什么开头的
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day06/wangyi_football.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Created by 秋叶夏风
+
+# 本模块的功能:<>
+
+from requests import get
+from lxml import etree
+## 我们公司就是一个做足球相关app的公司
+# 1.足球相关新闻
+# 2. 足球赛事
+# 3. 足球锦囊
+#
+# 这个函数是负责下载详情页的相关信息的
+
+def get_detailed_page_info(url):
+    # 爬虫第三步
+    response = get(url)
+    # with open('163.html','wb') as f:
+    #     f.write(response.content)
+    # 你拿到页面的第一步就是用withopen 去下载一下这个信息
+    # 看一下和浏览器中的内容是否一样,
+    # 要是不一样,那就要加useragent reference cookie等信息了
+
+    # 爬虫第四步
+    # 获取具体信息
+    html_ele = etree.HTML(response.text)
+    # / html / body / div[4] / div / div[2] / div[2] / a
+    event_name = html_ele.xpath('//div[@class="leftList2"]/div[@class="c2"]/a/text()')[0]
+    print(event_name)
+
+    # 表格里面的xpath,获取所有的tr://div[@class="leftList4"]/table/tbody/tr
+
+    # 这个tbody是浏览器自动添加的标签,
+    # 所以爬之前先下载下来,再说
+    # 审查元素和查看网页源代码是不一样的.
+    tr_ele_list = html_ele.xpath('//div[@class="leftList4"]/table/tr')
+    print(tr_ele_list)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    url = 'http://goal.sports.163.com/schedule/20170806.html'
+    get_detailed_page_info(url)
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+       ┌─┐       ┌─┐ + +
+    ┌──┘ ┴───────┘ ┴──┐++
+    │                 │
+    │       ───       │++ + + +
+    ███████───███████ │+
+    │                 │+
+    │       ─┴─       │
+    │                 │
+    └───┐         ┌───┘
+        │         │
+        │         │   + +
+        │         │
+        │         └──────────────┐
+        │                        │
+        │                        ├─┐
+        │                        ┌─┘
+        │                        │
+        └─┐  ┐  ┌───────┬──┐  ┌──┘  + + + +
+          │ ─┤ ─┤       │ ─┤ ─┤
+          └──┴──┘       └──┴──┘  + + + +
+                 神兽保佑
+                代码无BUG!
+
+
+'''
